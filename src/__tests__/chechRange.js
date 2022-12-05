3ff69c029cd0b74d4bb784f432b6b41003b78026
Anton <antonch92@gmail.com>
2022-12-05 16:30:14 +0100

    first commit

---
+++ src/__tests__/chechRange.js
@@ -0,0 +1,17 @@
+import sortHealthRank from '../index';
+
+test('checking of correct setting healt rank', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expectHeroes = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = sortHealthRank(heroes);
+
+  expect(result).toEqual(expectHeroes);
+});
